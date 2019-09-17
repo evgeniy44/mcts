c20dc90ae60119378add1c27bdad26edb2a1cd01
yevhenii <evgeniy.berlog@gmail.com>
2019-09-17 21:48:18 +0300

    state encoder

---
+++ src/checkers/state_encoder.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+class StateEncoder:
+
+	def __init__(self):
+		pass
+
+	def encode(self, state):
+		encoded_state = np.zeros(32)
+
+		for piece in state.board.pieces:
+			if state.whose_turn() == 1:
+				if piece.player == state.whose_turn():
+					if piece.king:
+						encoded_state[piece.position - 1] = 2
+					else:
+						encoded_state[piece.position - 1] = 1
+				else:
+					if piece.king:
+						encoded_state[piece.position - 1] = -2
+					else:
+						encoded_state[piece.position - 1] = -1
+			else:
+				if piece.player == state.whose_turn():
+					if piece.king:
+						encoded_state[32 - piece.position] = 2
+					else:
+						encoded_state[32 - piece.position] = 1
+				else:
+					if piece.king:
+						encoded_state[32 - piece.position] = -2
+					else:
+						encoded_state[32 - piece.position] = -1
+		return encoded_state
---
+++ src/checkers/test_stateEncoder.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+import numpy as np
+
+from src.checkers.game import Game
+from src.checkers.state_encoder import StateEncoder
+
+
+class TestStateEncoder(TestCase):
+
+	def test_encode_white(self):
+		game = Game()
+		encoder = StateEncoder()
+
+		encoded_state = encoder.encode(game)
+		self.assertTrue(np.array_equal(encoded_state, np.array([
+			1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			-1, -1, -1, - 1, -1, -1, -1, -1, -1, -1, -1, -1
+		])))
+
+	def test_encode_black(self):
+		game = Game()
+		game = game.move(game.get_possible_moves()[0])
+		encoder = StateEncoder()
+
+		encoded_state = encoder.encode(game)
+		self.assertTrue(np.array_equal(encoded_state, np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+																0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, 0, -1, -1, -1, -1,
+																-1, -1, -1, -1])))
