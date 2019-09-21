0a7f6fbcc483887f5841539af3538d222e3ebe85
yevhenii <evgeniy.berlog@gmail.com>
2019-09-21 12:55:41 +0300

    resolve distance as well

--- src/checkers/direction_resolver.py
+++ src/checkers/direction_resolver.py
@@ -9,20 +9,20 @@ class DirectionResolver:
 	def __init__(self):
 		pass
 
-	def resolve_direction(self, move):
+	def resolve_direction_and_distance(self, move):
 		x1, y1 = self.__get_coordinates(move[0])
 		x2, y2 = self.__get_coordinates(move[1])
 
 		if abs(x2 - x1) != abs(y2 - y1):
 			raise Exception("looks like illegal move: " + str(move))
 		if x2 > x1 and y2 > y1:
-			return NORTH_EAST
+			return NORTH_EAST, abs(x2 - x1)
 		if x2 > x1 and y2 < y1:
-			return SOUTH_EAST
+			return SOUTH_EAST, abs(x2 - x1)
 		if x2 < x1 and y2 < y1:
-			return SOUTH_WEST
+			return SOUTH_WEST, abs(x2 - x1)
 		if x2 < x1 and y2 > y1:
-			return NORTH_WEST
+			return NORTH_WEST, abs(x2 - x1)
 
 	def __get_coordinates(self, position):
 		y = (position - 1) // 4
